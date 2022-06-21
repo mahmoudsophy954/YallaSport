c1c590cf67cca948d7c8f5871db75bbefa62308a
3zzam <mahmoudazzam954@gmail.com>
2022-06-21 13:55:27 +0200

    Initial Commit

---
+++ YallaSoprtsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YallaSoprtsApp
+//
+//  Created by A on 31/03/1401 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
